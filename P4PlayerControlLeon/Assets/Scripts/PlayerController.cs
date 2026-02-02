7d7ea8ef3abadf2f4063617357fd6998efb4b285
765018-stack <765018@student.dusd.net>
2026-02-02 11:57:52 -0800

    Created Project
    create a camara move

---
+++ P4PlayerControlLeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'll move the vehicle forwar
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
