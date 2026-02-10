c5cda0a469ede9f36a3e0b5bafc947776e94af77
765018-stack <765018@student.dusd.net>
2026-02-10 11:43:49 -0800

    finished my project

--- P4PlayerControlLeon/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlLeon/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
 
     private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    private float turnSpeed = 100.0f;
     private float horizontalInput;
     private float forwardInput;
     
@@ -18,10 +18,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("vertical");
+        forwardInput = Input.GetAxis("Vertical");
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput); 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
     }
 }
